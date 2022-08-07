23a039a6185d38274dc65e0d67bcd7e3e23b6426
Arshad Ismail <arshad@jinfo.lk>
2022-08-08 00:59:26 +0530

    first commit

--- android/app/src/main/java/com/admireappsolutions/kathambam/MainActivity.java
+++ android/app/src/main/java/com/admireappsolutions/kathambam/MainActivity.java
@@ -1,4 +1,4 @@
-package com.kathambam;
+package com.admireappsolutions.kathambam;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/admireappsolutions/kathambam/MainApplication.java
+++ android/app/src/main/java/com/admireappsolutions/kathambam/MainApplication.java
@@ -1,4 +1,4 @@
-package com.kathambam;
+package com.admireappsolutions.kathambam;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.kathambam.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.admireappsolutions.kathambam.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
